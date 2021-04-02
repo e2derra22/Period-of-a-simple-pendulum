d2d3f5abfec33c9fe52c9d364f24bef2ebe093af
e2derra22 <raskindjoshua@gmail.com>
2021-04-02 16:15:14 +0200

    Rewrote it to graph periods instead of movement

--- main.py
+++ main.py
@@ -2,29 +2,40 @@ import numpy as np
 from numpy import sin, cos
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
+import math
 
-def equations(y0, t):
-    theta, x = y0
-    f = [x, -(g/l) * sin(theta)]
-    return f
+result_list = []
 
-def plot_results(time, theta1):
-    plt.plot(time, theta1[:,0])
-    s = '(Initial Angle = ' + str(initial_angle) + ' degrees)'
-    plt.title('Pendulum motion: ' + s)
-    plt.xlabel('time(s)')
-    plt.ylabel('angle(rad)')
-    plt.grid(True)
+def period(l, g, initial_angle):
+    return 2 * math.pi * math.sqrt(l/g) * math.sin(initial_angle)
 
 g = float(input('Enter the gravitational acceleration: '))
-l = float(input('Enter the lenght of the pendulum (cm): ')) * 100
+L = float(input('Enter the lenght of the pendulum (cm): ')) / 100
 initial_angle = float(input('Enter the initial angle: '))
 
-time = np.arange(0, 10.0, 0.025)
-theta0 = np.radians(initial_angle)
-x0 = np.radians(0.0)
-theta1 = odeint(equations, [theta0, x0], time) 
+for i in range(4):
+    result_list.append(period(L, g, initial_angle))
 
-plot_results(time, theta1)
+    if L > 0.2: 
+        L -= 0.1
 
+temp = [50, 40, 30, 20]
+plt.scatter(result_list, temp)
+plt.plot(result_list, temp, label='Actual')
+plt.plot(np.unique(result_list), np.poly1d(np.polyfit(result_list, temp, 1))(np.unique(result_list)), color='red', label='Best fit')
+s = '(Initial Angle = ' + str(initial_angle) + ' degrees)'
+plt.title('Pendulum motion: ' + s)
+plt.xlabel('Period of one oscillation (s)')
+plt.ylabel('lenght (cm)')
+plt.legend()
+plt.grid(True)
+
+gradient = np.polyfit(result_list, temp, 1)
+
+print(result_list)
+print(f'Gradient = {gradient}')
+
+for i in result_list:
+    print(i * 20)
 plt.show()
+
